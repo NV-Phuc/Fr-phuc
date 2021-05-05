aa48b06dcfea484060c76b72846584cff1e081b1
Phuc <1720176.nguyenvanphuc@gmail.com>
2021-05-05 19:54:56 +0700

    them file

--- b8/style.js
+++ b8/style.js
@@ -1,12 +1,13 @@
 var max = undefined;
-var arrNumber = [
-    2, 13, 24, 4, 7, 43, 36, 1, 0, 27, 9, 8, 37, 40
-];
+var arrNumber = [ 2, 13, 24, 4, 7, 43, 36, 1, 0, 27, 9, 8, 37, 40];
+function LargerNumber(arrNumber){
 max = arrNumber.length[0];
 for(var i = 1; i <= arrNumber.length; i++){
-    arrNumber[i] >= max;
-    max = (arrNumber[i]);
-    max ;
-    // console.log('Gia tri lon nhat trong mang la :', max);
+if(arrNumber[i] >= max){
+    max = arrNumber[i];
+    
+}
+   return max ; 
 }
 console.log('Gia tri lon nhat trong mang la :', max);
+}
